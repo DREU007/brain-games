6668a9cde0c3c79fae8f73b6c12ab37db5fc76c1
Denis Bolshakov <dreu007@gmail.com>
2023-01-25 22:32:19 +0400

    Feat calc_game.py

--- brain_games/calc_game.py
+++ brain_games/calc_game.py
@@ -1,17 +1,27 @@
 """Calc brain game.
 
-Player has to calculate values and give his answer.
+Player has to calculate values and give his result.
 """
-from brain_games.common_core import welcome_new_user, user_reply
+from brain_games.common_core import core_algorithm
+from random import randint, choice
+
 
 def single_calc_game():
     """Create a question for the user and return correct answer"""
-
+    val1, val2 = [randint(1, 100) for _ in range(2)]
+    available_operators = ['*', '+', '-']
+    operator = choice(available_operators)
+    if operator == '*':
+        result = val1 * val2
+    elif operator == '+':
+        result = val1 + val2
+    else:
+        result = val1 - val2
+    invite_msg = ('What is the result of the expression?'
+                  f'Question: {val1} {operator} {val2}')
+    print(invite_msg)
+    return result
 
 
 def main():
-    name = welcome_new_user()
-    cycles = 3
-    for cycle in range(cycles):
-        answer = single_calc_game()
-        user_reply(answer, name)
+    core_algorithm(single_calc_game())
