82380780e70232d17d79c6c37f7193bfa7478c57
Denis Bolshakov <dreu007@gmail.com>
2023-01-26 07:55:32 +0400

    Update games location

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Script to run brain_game.calc_game.py."""
-from brain_games.calc_game import main
+from brain_games.games.calc_game import main
 
 if __name__ == '__main__':
     main()
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Script to run brain_games.even_game."""
 
-from brain_games.even_game import main
+from brain_games.games.even_game import main
 
 if __name__ == '__main__':
     main()
