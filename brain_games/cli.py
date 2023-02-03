8000451aa812177c572113e32ceafd9649c4f35d
Denis Bolshakov <dreu007@gmail.com>
2023-02-03 10:43:53 +0400

    Refactor cli.py braing_games.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,11 +1,10 @@
 """Invite user to play brain-games."""
-from sys import stdout
-
 from prompt import string
 
 
 def welcome_user():
     """Request user to enter his 'name' and return personal welcome msg."""
     name = string('May I have your name? ')
-    welcome_reply = 'Hello, {0}!\n'.format(name)
-    stdout.write(welcome_reply)
+    welcome_reply = (f'Hello, {name}!\n'
+                     'Welcome to the Brain Games!')
+    print(welcome_reply)
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python3
 """This is a script to run brain-games in global environment."""
-
-
-from sys import stdout
-
 from brain_games.cli import welcome_user
 
 
 def main():
     """Start brain-games and invite user by asking his name."""
-    welcome_msg = 'Welcome to the Brain Games!\n'
-    stdout.write(welcome_msg)
     welcome_user()
 
 
