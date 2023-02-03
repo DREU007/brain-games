e38f7ec9bd3fb537d9bfdbb845a12809d131a86b
Denis Bolshakov <dreu007@gmail.com>
2023-02-03 15:43:31 +0400

    Refactor brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """Script to run brain_game.calc_game.py."""
-from brain_games.games.calc_game import main
+from brain_games.common_core import run_game
+from brain_games.games.calc_game import give_task, assign_question_answer
+
+
+def main():
+    run_game(give_task, assign_question_answer)
+
 
 if __name__ == '__main__':
     main()
