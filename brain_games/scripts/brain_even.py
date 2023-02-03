da3b6a25b86efae4335e911f597f0b4f07a628b1
Denis Bolshakov <dreu007@gmail.com>
2023-02-03 15:43:50 +0400

    Refactor brain_even.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """Script to run brain-even in global environment."""
+from brain_games.common_core import run_game
+from brain_games.games.even_game import give_task, assign_question_answer
 
-from brain_games.games.even_game import main
+
+def main():
+    run_game(give_task, assign_question_answer)
+
 
 if __name__ == '__main__':
     main()
