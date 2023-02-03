84a58f11f7122d25777c34272d5a7ceb2ee2730b
Denis Bolshakov <dreu007@gmail.com>
2023-02-03 15:44:15 +0400

    Refactor brain_prime.py

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 """Script to run brain_prime in global environment."""
+from brain_games.common_core import run_game
+from brain_games.games.prime_game import give_task, assign_question_answer
 
-from brain_games.games.prime_game import main
+
+def main():
+    run_game(give_task, assign_question_answer)
+
 
 if __name__ == '__main__':
     main()
