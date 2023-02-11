7baea1673c986a96e7ed88a9427447bf05aaf160
Denis Bolshakov <dreu007@gmail.com>
2023-02-12 00:32:49 +0400

    Refactor calc_game.py

--- brain_games/games/calc_game.py
+++ brain_games/games/calc_game.py
@@ -3,6 +3,7 @@
 Player has to calculate values and give his result.
 """
 from random import randint, choice
+from operator import add, sub, mul
 
 TASK_MSG = 'What is the result of the expression?'
 
@@ -10,14 +11,12 @@ TASK_MSG = 'What is the result of the expression?'
 def get_question_answer():
     """Return a question and correct answer"""
     val1, val2 = [randint(1, 100) for _ in range(2)]
-    available_operators = ['*', '+', '-']
-    operator = choice(available_operators)
-    if operator == '*':
-        result = val1 * val2
-    elif operator == '+':
-        result = val1 + val2
-    else:
-        result = val1 - val2
-
-    question_msg = f'{val1} {operator} {val2}'
+    operations = (
+        ('+', add),
+        ('-', sub),
+        ('*', mul)
+    )
+    operation_name, operation_method = choice(operations)
+    question_msg = f'{val1} {operation_name} {val2}'
+    result = operation_method(val1, val2)
     return question_msg, str(result)
