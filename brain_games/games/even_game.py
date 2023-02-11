bbc6f241d2158c2313edba8412106640112af5eb
Denis Bolshakov <dreu007@gmail.com>
2023-02-12 00:40:26 +0400

    Refactor even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -7,17 +7,10 @@ from random import randint
 TASK_MSG = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
-def create_number_and_is_it_even(range_start=1, range_finish=1_000):
-    """Return integer and is it even or not (True/False)."""
-    number = randint(range_start, range_finish)
-    answer_is_even = 'yes'
-    if number % 2:
-        answer_is_even = 'no'
-    return number, answer_is_even
-
-
 def get_question_answer():
     """Return a question and correct answer"""
-    number, cor_answer = create_number_and_is_it_even()
+    start, finish = 1, 1_000
+    number = randint(start, finish)
     question_msg = f'{number}'
-    return question_msg, cor_answer
+    answer = 'no' if question_msg % 2 else 'yes'
+    return question_msg, answer
