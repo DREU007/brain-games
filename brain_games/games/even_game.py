76e28d15ca3d393dab885da82bcc83d486651b59
Denis Bolshakov <dreu007@gmail.com>
2023-02-16 12:12:10 +0400

    Refactor even_game.py

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -9,8 +9,8 @@ TASK_MSG = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_question_answer():
     """Return a question and correct answer"""
-    start, finish = 1, 1_000
-    number = randint(start, finish)
+    number = randint(1, 1000)
+
     question_msg = f'{number}'
     answer = 'no' if number % 2 else 'yes'
     return question_msg, answer
